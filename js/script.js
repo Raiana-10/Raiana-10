7818a1b29ef3cbb74815359d1dbe67df1c4c7d90
Raiana-10 <contatestedopc3@gmail.com>
2024-11-06 21:34:14 -0300

    Add files via upload
    upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const pessoa = document.querySelector('.pessoa');
+    const quadrado = document.querySelector('.quadrado');
+
+    const jump = () => {
+        pessoa.classList.add('jump');
+
+        setTimeout(() => {
+            pessoa.classList.remove('jump');
+        }, 500);
+    }
+
+    const loop = setInterval(() => {
+        const quadradoPosition = quadrado.offsetLeft;
+        pessoaPosition = +window.getComputedStyle(pessoa).bottom.replace('px', '');
+
+        if (quadradoPosition < 120 && quadradoPosition > 0
+            && pessoaPosition < 80){
+
+            quadrado.style.animation = 'none'
+            quadrado.style.left = `${quadradoPosition}px`;
+
+            pessoa.style.animation = 'none'
+            pessoa.style.bottom = `${pessoaPosition}px`;
+
+            pessoa.src = 'img/tumulo.png';
+            pessoa.style.width = '120px'
+
+            clearInterval(loop);
+
+
+
+
+        }
+        
+    }, 10);
+
+    document.addEventListener('keydown', jump);
+    document.addEventListener('touchstart', jump);
+});
